90ade51ce4685937a2b7d9df5277e5429dfe58bc
iClusterDev <cluster.coredev@gmail.com>
2021-03-21 06:57:31 +0000

    map

--- client/src/scripts/app.js
+++ client/src/scripts/app.js
@@ -1,61 +1,119 @@
 import Engine from './Engine';
 import playerSrc from '../images/player.png';
 
+// ==========================================================
+// display
+// ==========================================================
 const canvas = document.querySelector('#canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 const ctx = canvas.getContext('2d');
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
 
 const images = {};
 images.player = new Image();
 images.player.src = playerSrc;
 
-function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
-  ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
-}
-
 function randomBetween(min, max) {
   return Math.floor(Math.random() * max - min);
 }
 
-const characterActions = ['up', 'right', 'down', 'left'];
+// const characterActions = ['up', 'right', 'down', 'left'];
+const characterActions = ['up', 'right', 'down'];
+const animationMap = [
+  {
+    name: 'up',
+    mirror: false,
+    frame: 3,
+    start: 3,
+    end: 13,
+    row: 0,
+  },
+  {
+    name: 'right',
+    mirror: false,
+    frame: 3,
+    start: 3,
+    end: 13,
+    row: 3,
+  },
+  {
+    name: 'down',
+    mirror: false,
+    frame: 0,
+    start: 0,
+    end: 12,
+    row: 6,
+  },
+  {
+    name: 'left',
+    mirror: true,
+    frame: 3,
+    start: 3,
+    end: 13,
+    row: 3,
+  },
+];
 
 class Character {
-  constructor(spriteSheet) {
+  constructor(spriteSheet, animationMap, context) {
+    this.context = context;
     this.spriteSheet = spriteSheet;
-    this.width = 103.0625;
-    this.height = 113.125;
+    this.width = 103.0625; // computed from spriteSheet / xFrames
+    this.height = 113.125; // computed from spriteSheet / yFrames
     this.speed = 5;
-    this.x = randomBetween(0, canvas.width - this.width);
-    this.y = randomBetween(0, canvas.height - this.height);
-    this.action =
-      characterActions[Math.floor(Math.random() * characterActions.length)];
-    if (this.action === 'up') {
-      this.frameX = 3;
-      this.frameY = 0;
-    }
-    if (this.action === 'right') {
-      this.frameX = 3;
-      this.frameY = 3;
-    }
-    if (this.action === 'left') {
-      this.frameX = 3;
-      this.frameY = 3;
-    }
-    if (this.action === 'down') {
-      this.frameX = 0;
-      this.frameY = 6;
+    this.x = randomBetween(0, this.context.canvas.width - this.width);
+    this.y = randomBetween(0, this.context.canvas.height - this.height);
+    this.action = characterActions[randomBetween(0, characterActions.length)];
+    this.animation = animationMap.find(
+      (animation) => animation.name === this.action
+    );
+  }
+
+  moveUp() {
+    if (this.y < 0 - this.height) {
+      this.y = this.context.canvas.height;
+      this.x = randomBetween(0, this.context.canvas.width - this.width);
+    } else {
+      this.y -= this.speed;
     }
   }
-  draw() {
-    if (this.action === 'left') {
-      // reverse the left frames
+
+  moveRight() {
+    if (this.x > this.context.canvas.width) {
+      this.x = 0 - this.width;
+      this.y = randomBetween(0, this.context.canvas.height - this.height);
+    } else {
+      this.x += this.speed;
     }
-    drawSprite(
+  }
+
+  moveDown() {
+    if (this.y > this.context.canvas.height) {
+      this.y = 0 - this.height;
+      this.x = randomBetween(0, this.context.canvas.width - this.width);
+    } else {
+      this.y += this.speed;
+    }
+  }
+
+  moveLeft() {
+    if (this.x < 0 - this.width) {
+      this.x = this.context.canvas.width;
+      this.y = randomBetween(0, this.context.canvas.height - this.height);
+    } else {
+      this.x -= this.speed;
+    }
+  }
+
+  draw() {
+    this.context.drawImage(
       this.spriteSheet,
-      this.width * this.frameX,
-      this.height * this.frameY,
+      this.width * this.animation.frame,
+      this.height * this.animation.row,
       this.width,
       this.height,
       this.x,
@@ -66,70 +124,33 @@ class Character {
   }
 
   update() {
-    if (this.action === 'up') {
-      if (this.y < 0 - this.height) {
-        this.y = canvas.height;
-        this.x = randomBetween(0, canvas.width - this.width);
-      } else {
-        this.y -= this.speed;
-      }
-
-      if (this.frameX < 13) {
-        this.frameX++;
-      } else {
-        this.frameX = 3;
-      }
+    switch (this.action) {
+      case 'up':
+        this.moveUp();
+        break;
+      case 'right':
+        this.moveRight();
+        break;
+      case 'down':
+        this.moveDown();
+        break;
+      default:
+        console.log('Error: Update()');
     }
 
-    if (this.action === 'right') {
-      if (this.x > canvas.width) {
-        this.x = 0 - this.width;
-        this.y = randomBetween(0, canvas.height - this.height);
-      } else {
-        this.x += this.speed;
-      }
-
-      if (this.frameX < 13) {
-        this.frameX++;
-      } else {
-        this.frameX = 3;
-      }
-    }
-
-    if (this.action === 'down') {
-      if (this.y > canvas.height) {
-        this.y = 0 - this.height;
-        this.x = randomBetween(0, canvas.width - this.width);
-      } else {
-        this.y += this.speed;
-      }
-
-      if (this.frameX < 12) {
-        this.frameX++;
-      } else {
-        this.frameX = 0;
-      }
-    }
-
-    if (this.action === 'left') {
-      if (this.x < 0 - this.width) {
-        this.x = canvas.width;
-        this.y = randomBetween(0, canvas.height - this.height);
-      } else {
-        this.x -= this.speed;
-      }
-
-      this.frameX = 3;
-      this.frameY = 3;
+    if (this.animation.frame < this.animation.end) {
+      this.animation.frame++;
+    } else {
+      this.animation.frame = this.animation.start;
     }
   }
 }
 
 images.player.addEventListener('load', (e) => {
   const characters = [];
-  const numberOfCharacters = 10;
+  const numberOfCharacters = 1;
   for (let i = 0; i < numberOfCharacters; i++) {
-    characters.push(new Character(images.player));
+    characters.push(new Character(images.player, animationMap, ctx));
   }
 
   // ==========================================================
@@ -148,14 +169,6 @@ images.player.addEventListener('load', (e) => {
       }
     }
   );
-
-  // ==========================================================
-  // listeners
-  // ==========================================================
-  window.addEventListener('resize', () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  });
 
   engine.start();
 });
