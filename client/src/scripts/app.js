cd02c37bda80a7f7a5cfe9c7cdf243ce7dde46dc
iClusterDev <cluster.coredev@gmail.com>
2021-03-21 18:04:02 +0000

    split start

--- client/src/scripts/app.js
+++ client/src/scripts/app.js
@@ -1,5 +1,14 @@
+import characterSrc from '../images/character2.png';
 import Engine from './Engine';
-import playerSrc from '../images/player.png';
+
+// ==========================================================
+// resource load
+// ==========================================================
+const spriteSheet = new Image();
+spriteSheet.src = characterSrc;
+spriteSheet.onload = function () {
+  init();
+};
 
 // ==========================================================
 // display
@@ -13,162 +22,64 @@ window.addEventListener('resize', () => {
   canvas.height = window.innerHeight;
 });
 
-const images = {};
-images.player = new Image();
-images.player.src = playerSrc;
+// ==========================================================
+// globals
+// ==========================================================
+const scale = 2;
+const width = 16;
+const height = 18;
+const scaledWidth = scale * width;
+const scaledHeight = scale * height;
 
-function randomBetween(min, max) {
-  return Math.floor(Math.random() * max - min);
-}
-
-// const characterActions = ['up', 'right', 'down', 'left'];
-const characterActions = ['up', 'right', 'down'];
-const animationMap = [
-  {
-    name: 'up',
-    mirror: false,
-    frame: 3,
-    start: 3,
-    end: 13,
-    row: 0,
-  },
-  {
-    name: 'right',
-    mirror: false,
-    frame: 3,
-    start: 3,
-    end: 13,
-    row: 3,
-  },
-  {
-    name: 'down',
-    mirror: false,
-    frame: 0,
-    start: 0,
-    end: 12,
-    row: 6,
-  },
-  {
-    name: 'left',
-    mirror: true,
-    frame: 3,
-    start: 3,
-    end: 13,
-    row: 3,
-  },
-];
-
-class Character {
-  constructor(spriteSheet, animationMap, context) {
-    this.context = context;
-    this.spriteSheet = spriteSheet;
-    this.width = 103.0625; // computed from spriteSheet / xFrames
-    this.height = 113.125; // computed from spriteSheet / yFrames
-    this.speed = 5;
-    this.x = randomBetween(0, this.context.canvas.width - this.width);
-    this.y = randomBetween(0, this.context.canvas.height - this.height);
-    this.action = characterActions[randomBetween(0, characterActions.length)];
-    this.animation = animationMap.find(
-      (animation) => animation.name === this.action
-    );
-  }
-
-  moveUp() {
-    if (this.y < 0 - this.height) {
-      this.y = this.context.canvas.height;
-      this.x = randomBetween(0, this.context.canvas.width - this.width);
-    } else {
-      this.y -= this.speed;
-    }
-  }
-
-  moveRight() {
-    if (this.x > this.context.canvas.width) {
-      this.x = 0 - this.width;
-      this.y = randomBetween(0, this.context.canvas.height - this.height);
-    } else {
-      this.x += this.speed;
-    }
-  }
-
-  moveDown() {
-    if (this.y > this.context.canvas.height) {
-      this.y = 0 - this.height;
-      this.x = randomBetween(0, this.context.canvas.width - this.width);
-    } else {
-      this.y += this.speed;
-    }
-  }
-
-  moveLeft() {
-    if (this.x < 0 - this.width) {
-      this.x = this.context.canvas.width;
-      this.y = randomBetween(0, this.context.canvas.height - this.height);
-    } else {
-      this.x -= this.speed;
-    }
-  }
-
-  draw() {
-    this.context.drawImage(
-      this.spriteSheet,
-      this.width * this.animation.frame,
-      this.height * this.animation.row,
-      this.width,
-      this.height,
-      this.x,
-      this.y,
-      this.width,
-      this.height
-    );
-  }
-
-  update() {
-    switch (this.action) {
-      case 'up':
-        this.moveUp();
-        break;
-      case 'right':
-        this.moveRight();
-        break;
-      case 'down':
-        this.moveDown();
-        break;
-      default:
-        console.log('Error: Update()');
-    }
-
-    if (this.animation.frame < this.animation.end) {
-      this.animation.frame++;
-    } else {
-      this.animation.frame = this.animation.start;
-    }
-  }
-}
-
-images.player.addEventListener('load', (e) => {
-  const characters = [];
-  const numberOfCharacters = 1;
-  for (let i = 0; i < numberOfCharacters; i++) {
-    characters.push(new Character(images.player, animationMap, ctx));
-  }
-
-  // ==========================================================
-  // engine
-  // ==========================================================
-  const engine = new Engine(
-    (timestep) => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      for (let i = 0; i < characters.length; i++) {
-        characters[i].update();
-      }
-    },
-    () => {
-      for (let i = 0; i < characters.length; i++) {
-        characters[i].draw();
-      }
-    }
+function drawFrame(frameX, frameY, canvasX, canvasY) {
+  ctx.drawImage(
+    spriteSheet,
+    frameX * width,
+    frameY * height,
+    width,
+    height,
+    canvasX,
+    canvasY,
+    scaledWidth,
+    scaledHeight
   );
+}
 
+let animationCycle = [0, 1, 0, 2];
+let currentAnimationCycleIndex = 0;
+let upCycle = 1;
+let rightCycle = 2;
+let downCycle = 0;
+let leftCycle = 3;
+let frameCount = 0;
+
+// ==========================================================
+// engine
+// ==========================================================
+function update() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  frameCount++;
+  if (frameCount === 10) {
+    frameCount = 0;
+    currentAnimationCycleIndex++;
+    if (currentAnimationCycleIndex >= animationCycle.length) {
+      currentAnimationCycleIndex = 0;
+    }
+  }
+}
+
+function render() {
+  drawFrame(animationCycle[currentAnimationCycleIndex], upCycle, 200, 200);
+  drawFrame(animationCycle[currentAnimationCycleIndex], rightCycle, 300, 300);
+  drawFrame(animationCycle[currentAnimationCycleIndex], downCycle, 100, 100);
+  drawFrame(animationCycle[currentAnimationCycleIndex], leftCycle, 400, 400);
+}
+
+const engine = new Engine(update, render);
+
+// ==========================================================
+// init
+// ==========================================================
+function init() {
   engine.start();
-});
+}
