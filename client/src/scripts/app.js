36e828402906cd876ed005ac93b4417f075c58a7
iClusterDev <cluster.coredev@gmail.com>
2021-04-13 17:05:19 +0100

    demo updated

--- client/src/scripts/app.js
+++ client/src/scripts/app.js
@@ -3,16 +3,11 @@ import Display from './core/Display';
 import Resource from './core/Resource';
 
 import heroSpriteSheetSrc from '../images/hero.png';
-import blockSpriteSheetSrc from '../images/block.png';
 
 import Hero from './assets/Hero';
-import Enemy from './assets/Enemy';
 
 export default () => {
-  const IMAGES = [
-    { src: heroSpriteSheetSrc, name: 'heroSpriteSheet' },
-    { src: blockSpriteSheetSrc, name: 'blockSpriteSheet' },
-  ];
+  const IMAGES = [{ src: heroSpriteSheetSrc, name: 'heroSpriteSheet' }];
 
   Resource.preloadImages(IMAGES).then(() => {
     // setup
@@ -40,33 +35,14 @@ export default () => {
       ],
     });
 
-    const enemy = new Enemy({
-      name: 'enemy',
-      position: { x: 0, y: 0 },
-      graphics: {
-        spriteSheet: Resource.getImage('blockSpriteSheet'),
-        columns: 1,
-        rows: 1,
-        scale: 2,
-      },
-      input: [
-        { code: 'KeyW', action: 'up' },
-        { code: 'KeyS', action: 'down' },
-        { code: 'KeyA', action: 'right' },
-        { code: 'KeyD', action: 'left' },
-      ],
-    });
-
     // game loop
     const gameLoop = new Engine(
       function update(elapsedTime) {
         display.clear();
         hero.update(elapsedTime, 0, 0, display.width, display.height);
-        enemy.update(elapsedTime, 0, 0, display.width, display.height);
       },
       function render() {
         display.render(hero.sprite, hero.position.x, hero.position.y);
-        display.render(enemy.sprite, enemy.position.x, enemy.position.y);
       }
     );
     gameLoop.start();
